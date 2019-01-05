a88c78431934a4d05551b4d8abb6ff927722b36e
GKaszewski <gabrielkaszewski@gmail.com>
2019-01-05 23:39:33 +0100

    Added Engine class and Resource Manager

---
+++ BrickEngine/Core/Engine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SFML.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Window;
+
+namespace BrickEngine.Core
+{
+    class Engine
+    {
+        private RenderWindow _window;
+        public Engine(string title, Vector2u size, uint framerate, bool fullscreen)
+        {
+            var settings = new ContextSettings();
+
+            if (fullscreen)
+                settings.AntialiasingLevel = 8;
+
+            _window.SetTitle(title);
+            _window.Size = size;
+            _window.SetFramerateLimit(framerate);
+        }
+    }
+}
---
+++ BrickEngine/Core/ResourceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Audio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickEngine.Core
+{
+    class ResourceManager
+    {
+    }
+}
