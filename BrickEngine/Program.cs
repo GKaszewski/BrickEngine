100f53818f5f2e04d4b03a13713bd45300385413
GKaszewski <gabrielkaszewski@gmail.com>
2019-01-17 18:25:14 +0100

    Update Program.cs

--- BrickEngine/Program.cs
+++ BrickEngine/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BrickEngine.Core;
+using BrickEngine.Utility;
 
 namespace BrickEngine
 {
@@ -10,6 +7,9 @@ namespace BrickEngine
     {
         static void Main(string[] args)
         {
+            var engine = new Engine("GAME TITLE {BRICK ENGINE} IN DEVELOPMENT", new Vector2i(1280, 720), 60);
+
+            //using (Toolkit.Init()) new Engine("GAME TITLE {BRICK ENGINE} IN DEVELOPMENT", new Vector2i(1280, 720), 60);
         }
     }
 }
