81440527425376ad395adbae02a07175718a599f
GKaszewski <gabrielkaszewski@gmail.com>
2019-01-17 18:24:41 +0100

    Initialized camera

---
+++ BrickEngine/Core/Camera3D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickEngine.Core
+{
+    class Camera3D
+    {
+    }
+}
