3b158f3c9cd76b1297f09739a8ee84cd90218dbd
GKaszewski <gabrielkaszewski@gmail.com>
2019-01-17 19:56:02 +0100

    Update Texture.cs

--- BrickEngine/Utility/Texture.cs
+++ BrickEngine/Utility/Texture.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Remoting.Messaging;
+using OpenTK.Graphics.OpenGL4;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
 
 namespace BrickEngine.Utility
 {
     public struct Texture
     {
+        private int _id;
+        public int ID => _id;
+        public Vector2i size;
+
+        public Texture(string path)
+        {
+            _id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, _id);
+            Bitmap bitmap = new Bitmap(path);
+            size = new Vector2i(bitmap.Width, bitmap.Height);
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, size.X, size.Y), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, size.X, size.Y, 0, OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            bitmap.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+        }
+
     }
 }
