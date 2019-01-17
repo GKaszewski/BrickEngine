2e4164ca6453faaf0a77966fb5356573c84c7fa1
GKaszewski <gabrielkaszewski@gmail.com>
2019-01-17 18:22:51 +0100

    Added utilities

---
+++ BrickEngine/Utility/Vector2i.cs
@@ -0,0 +1,31 @@
+﻿namespace BrickEngine.Utility
+{
+    public struct Vector2i
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2i(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2i operator +(Vector2i vector)
+        {
+            var temp = new Vector2i();
+            temp.X += vector.X;
+            temp.Y += vector.Y;
+            return temp;
+        }
+
+        public static Vector2i operator -(Vector2i vector)
+        {
+            var temp = new Vector2i();
+            temp.X -= vector.X;
+            temp.Y -= vector.Y;
+            return temp;
+        }
+
+    }
+}
---
+++ BrickEngine/Utility/Vertex.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK;
+
+namespace BrickEngine.Utility
+{
+    public struct Vertex
+    {
+        private Vector2 _position;
+        public Vector2 Position
+        {
+            get => _position;
+            set => _position = value;
+        }
+
+        public const int SIZE = 2;
+
+        public Vertex(Vector2 position) => _position = position;
+        public Vertex(float x, float y) => _position = new Vector2(x,y);
+
+        public static float[] Process(Vertex[] vertices)
+        {
+            var count = 0;
+            var data = new float[vertices.Length * SIZE];
+
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                data[count] = vertices[i].Position.X;
+                data[count+1] = vertices[i].Position.Y;
+
+                count += 2;
+            }
+
+            return data;
+        }
+    }
+}
